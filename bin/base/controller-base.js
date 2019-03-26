69e53b277f7a2319cb7f532831d81a20f6134a65
Rafael Jovito <rafacj7@gmail.com>
2019-03-26 19:43:10 -0300

    [ADD] Model Usuario, Controllers Usuario, routes

--- bin/base/controller-base.js
+++ bin/base/controller-base.js
@@ -6,7 +6,7 @@ exports.post = async (repository, validationContract, req, res) =>{
         if(!validationContract.isValid()){
             res.status(400).send({
                 message: 'Existem dados inválidos na sua requisição',
-                validation: validationContract.error()
+                validation: validationContract.errors()
             }).end();
             return;
         }
@@ -28,7 +28,7 @@ exports.put = async (repository, validationContract, req, res) =>{
         if(!validationContract.isValid()){
             res.status(400).send({
                 message: 'Existem dados inválidos na sua requisição',
-                validation: validationContract.error()
+                validation: validationContract.errors()
             }).end();
             return;
         }
--- bin/express.js
+++ bin/express.js
@@ -6,6 +6,7 @@ const variables = require('../bin/configuration/variables');
 //routers
 const categoriaRouter = require('../routes/catergoria-router');
 const produtoRouter = require('../routes/produto-router');
+const usuarioRouter = require('../routes/usuario-router');
 
 //Criando/Invocando a Api/Server Web do Express
 const app = express();
@@ -16,12 +17,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //Configurando a conexão com banco de dados
 mongoose.connect(variables.Database.connection, { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
 
 //Configurando as rotas
 app.use('/api/categoria', categoriaRouter);
 app.use('/api/produto', produtoRouter);
+app.use('/api/usuario', usuarioRouter);
 
 
 //Exportando nossa Api
 module.exports = app;
+
+
+// Api -> MIDDLEWARES -> Rotas -> Controller -> Repository -> Banco
