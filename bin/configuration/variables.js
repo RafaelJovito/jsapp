a19758b10900af5736a69b8608ecbee2c8163f3c
Rafael Jovito <rafacj7@gmail.com>
2019-03-29 14:49:16 -0300

    [ADD] Middlewares, authenctication

--- bin/configuration/variables.js
+++ bin/configuration/variables.js
@@ -5,6 +5,9 @@ const variables = {
     Database: {
         connection: process.env.connection || 'mongodb://jovito:EwQcvabcDhJcD7n@ds119164.mlab.com:19164/nofoodjs'
                                              
+    },
+    Security: {
+        secretyKey: '06360038dbc2940b26be7b12339b618a|ee579026991c38b31769233d06321e55'
     }
 }
 module.exports = variables;
